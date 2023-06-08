ec0325e7b82052ac1632e4b5cb579f5439741d6e
Thomas Zwagerman <36264706+thomaszwagerman@users.noreply.github.com>
2023-06-08 16:42:38 +0100

    added package installation script for Georgia

---
+++ package_installation.R
@@ -0,0 +1,22 @@
+packages <- c(
+  "httr",
+  "XML",
+  "dplyr",
+  "rvest",
+  "leaflet",
+  "stringr",
+  "here",
+  "sf",
+  "dplyr",
+  "purrr",
+  "mapview",
+  "leafem",
+  "leaflet.extras",
+  "htmlwidgets",
+  "htmltools",
+  "readxl",
+  "tidyr",
+  "rmarkdown"
+)
+
+lapply(packages, install.packages)
